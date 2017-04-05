5eec7d8a56f398c16b35dcc1a4938e61a652a8eb
Jeremy Simon <jeremysimon2@gmail.com>
2017-04-04 21:22:47 -0400

    Initial package.js created

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "Francais-Pour-Tout",
+  "version": "0.0.1",
+  "description": "French learning tool for dynamic lesson creation and repition practice",
+  "dependencies": {
+    "express": "^4.10.2",
+    "socket.io": "^1.7.3"
+  },
+  "license": "UNLICENSED",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/jsimon1/FrancaisPourTout"
+  },
+  "scripts": {
+    "start": "node server.js"
+  },
+  "main": "French Learning Tool",
+  "author": "Jeremy Simon"
+}
