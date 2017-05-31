523f16f92b8dc260483a9f5ad3330a6de14dca01
Jeremy Simon <jeremysimon2@gmail.com>
2017-05-31 04:04:36 -0400

    Request to pages started for installation

---
+++ explorer.js
@@ -0,0 +1,23 @@
+//Explorer.js is used for installing the initial database
+console.log("Explorer Loaded");
+var request = require('request-promise');
+var cheerio =  require('cheerio');
+
+
+//getPage is a utility function to return a page given a link
+module.exports.getPage = (link) => {
+	console.log("Link:" + link);
+	var options = {
+		uri: link,
+		transform: function (body) {
+			return cheerio.load(body);
+		}
+	};
+	request(options)
+		.then(function ($) {
+			console.log($);
+		})
+		.catch(function (err){
+			console.log("Explorer failed at getPage");
+		});
+}
